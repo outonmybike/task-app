30e60b1b0cc61642a0b354717a1538bd7a44c4de
DAN NELSON <dannelson@DANs-Air.lan>
2020-11-22 14:06:18 -0700

    refactor js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,32 @@
-var formEl = document.querySelector('#task-form')
-// var buttonEl = document.querySelector('#save-task');
-var tasksToDoEl = document.querySelector('#tasks-to-do');
+var formEl = document.querySelector('#task-form') //selects form div
 
-var createTaskHandler = function() {
+var tasksToDoEl = document.querySelector('#tasks-to-do'); //selects ul
+
+var taskFormHandler = function(event) {
 	event.preventDefault();
 	var taskNameInput = document.querySelector('input[name="task-name"]').value;
 	var taskTypeInput = document.querySelector('select[name="task-type"]').value;
 
+	var taskDataObj = {
+		name: taskNameInput,
+		type: taskTypeInput
+	};
+
+	createTaskEl(taskDataObj);
+
+};
+
+var createTaskEl = function(taskDataObj) {
 	var listItemEl = document.createElement('li');
 	listItemEl.className = 'task-item';
 
 	var taskInfoEl = document.createElement('div');
 	taskInfoEl.className = 'task-info';
-	taskInfoEl.innerHTML = '<h3 class="task-name">' + taskNameInput + '</h3><span class="task-type">'+taskTypeInput+'</span>';
-
+	taskInfoEl.innerHTML = '<h3 class="task-name">' + taskDataObj.name + '</h3><span class="task-type">'+taskDataObj.type+'</span>';
 	listItemEl.appendChild(taskInfoEl);
-
 	tasksToDoEl.appendChild(listItemEl);
-};
+}
 
-formEl.addEventListener('submit',createTaskHandler);
-// buttonEl.addEventListener('click',createTaskHandler);
+
+formEl.addEventListener('submit',taskFormHandler);
+
