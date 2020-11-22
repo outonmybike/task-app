c8f3c1e94fe8bb2c8dd551ba9e7efb6d56cb4d12
DAN NELSON <dannelson@DANs-Air.lan>
2020-11-22 13:51:44 -0700

    updating js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
-var buttonEl = document.querySelector('#save-task');
+var formEl = document.querySelector('#task-form')
+// var buttonEl = document.querySelector('#save-task');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
 var createTaskHandler = function() {
+	event.preventDefault();
+	var taskNameInput = document.querySelector('input[name="task-name"]').value;
+	var taskTypeInput = document.querySelector('select[name="task-type"]').value;
+
 	var listItemEl = document.createElement('li');
 	listItemEl.className = 'task-item';
-	listItemEl.textContent = 'New Task';
+
+	var taskInfoEl = document.createElement('div');
+	taskInfoEl.className = 'task-info';
+	taskInfoEl.innerHTML = '<h3 class="task-name">' + taskNameInput + '</h3><span class="task-type">'+taskTypeInput+'</span>';
+
+	listItemEl.appendChild(taskInfoEl);
+
 	tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener('click',createTaskHandler);
+formEl.addEventListener('submit',createTaskHandler);
+// buttonEl.addEventListener('click',createTaskHandler);
