4cb16f58ae7b3179b491ccf8ffef86c9295529e4
DAN NELSON <dannelson@DANs-Air.lan>
2020-11-22 16:52:56 -0700

    edit task function started

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,17 +77,36 @@ var createTaskActions = function(taskId) {
 };
 
 var taskButtonHandler = function(event) {
+	var targetEl = event.target;
 	console.log(event.target);
 
-	if(event.target.matches('.delete-btn')) {
-		var taskId = event.target.getAttribute('data-task-id');
+	if(targetEl.matches('.edit-btn')) {
+		var taskId = targetEl.getAttribute('data-task-id');
+		editTask(taskId);
+	}
+
+	else if(targetEl.matches('.delete-btn')) {
+		var taskId = targetEl.getAttribute('data-task-id');
 		deleteTask(taskId);
 	}
+
 }
 
 var deleteTask = function(taskId) {
 	var taskSelected = document.querySelector('.task-item[data-task-id="'+taskId+'"]');
 	taskSelected.remove();
+}
+
+var editTask = function(taskId) {
+	console.log('ediing task: '+taskId)
+	var taskSelected = document.querySelector('.task-item[data-task-id="'+taskId+'"]');
+	var taskName = taskSelected.querySelector('h3.task-name').textContent;
+	var taskType = taskSelected.querySelector('span.task-type').textContent;
+	document.querySelector('input[name="task-name"]').value = taskName;
+	document.querySelector('select[name="task-type"]').value = taskType;
+	document.querySelector('#save-task').textContent='Update Task';
+	formEl.setAttribute('data-task-id',taskId);
+
 }
 
 
